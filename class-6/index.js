90d7df9d15e1cba96bcd493707d67a9040153c26
wajiha-kulsum <wajihakulsum786@gmail.com>
2026-09-24 02:59:15 +0530

    todos

--- class-6/index.js
+++ class-6/index.js
@@ -1,3 +1,5 @@
+let todoIndex = 1;
+ 
 function addTodo() {
     // write the code that reads the contents of input box
     //can create a new todo 
@@ -5,10 +7,31 @@ function addTodo() {
     const element = document.getElementById("todoInput")
     const todo = element.value;
 
-    const newDiv = document.createElement("div");
-    newDiv.innerHTML = todo ;
-
-    const parentDiv = document.getElementById("todos");
-    parentDiv.appendChild(newDiv);
     
+    const todoDiv = document.createElement("div")
+    todoDiv.setAttribute("id", "todo" + todoIndex);
+    
+    const todoSpan = document.createElement("span")
+    todoSpan.innerHTML = todo;
+    
+    todoDiv.appendChild(todoSpan)
+
+    const todoButton = document.createElement("button")
+    todoButton.innerHTML = " Delete Todo"
+    todoButton.setAttribute("onclick" , "deleteTodo(" + todoIndex + ")")
+
+    todoDiv.appendChild(todoButton)
+
+   document.getElementById("todos").appendChild(todoDiv)
+   todoIndex = todoIndex + 1; 
+
+    
+    
+}
+
+function deleteTodo(index){
+    // alert("delet todo called " + index)
+     const divElement = document.getElementById("todo" +  index)
+    //  divElement.parentElement.removeChild(divElement);
+    document.getElementById("todos").removeChild(divElement);
 }
